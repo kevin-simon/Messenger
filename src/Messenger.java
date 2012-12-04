faa824cf64fb01fc80742de5831ea7d51175ef6d
kevin-simon <kevin.simon@kekekiwi.fr>
2012-12-04 03:22:12 +0100

    Ajout des libs pour CouchDB

--- src/Messenger.java
+++ src/Messenger.java
@@ -1,21 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.URL;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.lightcouch.CouchDbClient;
 
-import com.google.gson.JsonObject;
-
-import ui.Window;
-import utils.Properties;
 import rmi.Type;
+import utils.Properties;
+
+import com.google.gson.JsonObject;
 
 public class Messenger {
 	
