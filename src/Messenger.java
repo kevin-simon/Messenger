d9dfde93a23ae4440c260578d0cadc154f3f6759
kevin-simon <kevin.simon@kekekiwi.fr>
2012-12-04 01:30:06 +0100

    Début de l'usage du broadcast pour détecter les machines utilisant l'application

--- src/Messenger.java
+++ src/Messenger.java
@@ -1,3 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.lightcouch.CouchDbClient;
+
+import com.google.gson.JsonObject;
+
 import ui.Window;
 import utils.Properties;
 import rmi.Type;
@@ -15,16 +30,63 @@ public class Messenger {
 	}
 	
 	public Messenger() {
+		this.peerType = Type.PEER;
 	}
 
-	public static void main(String[] args) {
-		Messenger messenger = Messenger.getInstance();
+	public static void main(String[] args) throws Exception {
+		/*Messenger messenger = Messenger.getInstance();
 		messenger.subscribe();
-		new Window("Messenger");
+		//new Window("Messenger");*/
+		
+		String Broadcastaddress = new String("192.168.1.255");
+	    int port = 2000;
+	    DatagramSocket serverSocket = new DatagramSocket();
+	    serverSocket.setBroadcast(true);
+	    InetAddress IPAddress = InetAddress.getByName(Broadcastaddress);
+	    System.out.println("Sending Discovery message to " + IPAddress + " Via UDP port " + port);
+
+	    byte[] sendData = new byte[4];
+	    sendData[0] = 'F';
+	    sendData[1] = 'I';
+	    sendData[2] = 'N';
+	    sendData[3] = 'D';
+
+	    DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,IPAddress,port);
+
+	    int i = 0;
+	    new Thread() {
+	        public void run() {
+	     
+	            try {
+	                int port = 2000;
+	                DatagramSocket dsocket = new DatagramSocket(port);
+	                byte[] buffer = new byte[2048];
+	     
+	                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+	                dsocket.receive(packet);
+	                System.out.println("Receiving...");
+	                String msg = new String(buffer, 0, packet.getLength());
+	                System.out.println(packet.getAddress().getHostAddress() + " : " + msg);
+	                packet.setLength(buffer.length);
+	                dsocket.close();
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }.start();
+	    serverSocket.send(sendPacket);
+	    System.out.println("Packet sent");
 	}
 	
 	public void subscribe() {
-		System.out.println("http://" + Properties.APP.get("subscribe_ip") + ":5984/MessengerApplication/peers");
+		CouchDbClient dbClient = new CouchDbClient("messenger_application", true, "http", Properties.APP.get("subscribe_ip"), 5984, null, null);
+		/*Map<String, Object> map = new HashMap<String, Object>();
+		map.put("_id", "peers");
+		map.put("192.168.1.8", this.peerType.toString());
+		dbClient.update(map);*/
+		JsonObject json = dbClient.find(JsonObject.class, "peers");
+		System.out.println(json);
+		
 	}
 
 }
