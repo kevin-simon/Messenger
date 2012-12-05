de5201a661b7271cf7e49c3e697cf3c60dcb0101
kevin-simon <kevin.simon@kekekiwi.fr>
2012-12-05 18:36:57 +0100

    Correction de l'erreur (absence du throws RemoteException)

--- src/rmi/interfaces/IPeer.java
+++ src/rmi/interfaces/IPeer.java
@@ -2,9 +2,10 @@ package rmi.interfaces;
 
 import java.net.InetAddress;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 public interface IPeer extends Remote {
 	
-	public void connectTo(ArrayList<InetAddress> superPeerAddresses);
+	public void connectTo(ArrayList<InetAddress> superPeerAddresses) throws RemoteException;
 }
